ef36c063965f1b6146acf8c31546f1b3ddf74926
Diego Gomez Chavarria <diegoxanderdx@gmail.com>
2023-08-24 11:16:39 -0500

    Update card_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 # el metodo "describe" como su nombre lo sugiere, describe que es lo que estamos testiando, en este caso la clase "Card", 
-# recibe como parametro un string con el nombre de la clase que estamos testeando, y un bloque de codigo que contiene los test que queremos realizar delimitados
+# recibe como parametro un string con el nombre de la clase que estamos testeando(aunque podemos invocarlo sin argumentos), y un bloque de codigo que contiene los test que queremos realizar delimitados
 # dentro de un do      end. en el ecosistema de Rspec un tes es llamado un example, un example de como debe funcionar. tambien podemos agrupar examples
 RSpec.describe 'Card' do
 

853452292e91206d1ee2db61883af86c2898150d
Diegomez-dev <diegoagc@yahoo.es>
2023-08-23 23:09:04 -0500

    expect and eq methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,6 +7,13 @@ Rspec.describe 'Card' do
   # y un bloque de codigo que contiene el test que queremos realizar delimitados dentro de un do      end.
   # tanto "describe" como 'it' estan disponibles de manera global en el ecosistema de Rspec
   it 'has a type' do
-
+    # asumimos que la clase Card ya esta definida y que tiene un metodo llamado type que retorna el tipo de la carta y 
+    # que podemos instanciar un objeto de la clase Card y asignarlo a la variable card
+    card = Card.new('Ace of Spades')
+    # con el metodo "expect" podemos definir una expectativa, que es una condicion que debe cumplir el test para que sea exitoso, puede recibir como argumento un 
+    # objeto, un atributo de un objeto, un metodo de un objeto, una expresion plana de ruby etc. en este caso recibe como argumento el objeto card
+    # con el metodo "eq" que es un matcher, que es un metodo que compara el resultado de un test con un valor esperado dado en el argumento, en este caso 
+    # el valor esperado es el string 'Ace of Spades', como resultado del type de la carta.
+    expect(card.type).to eq('Ace of Spades')
   end
 end

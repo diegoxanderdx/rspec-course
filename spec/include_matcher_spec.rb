4214629ac90ad282424ccea01fe5280fe3081146
Diegomez-dev <diegoagc@yahoo.es>
2023-08-27 18:46:46 -0500

    include matcher

---
+++ spec/include_matcher_spec.rb
@@ -0,0 +1,33 @@
+# el include matcher busca que dentro de una coleccion, exista un valor especifico, no importa el orden, puede ser un substring, un valor en un array o 
+# un key en un hash, un conjunto llave valor en un hash, etc.
+RSpec.describe 'include matcher' do
+  describe 'hot chocolate' do
+    it 'checks for substring inclusion' do
+      expect(subject).to include('hot')
+      expect(subject).to include('choc')
+      expect(subject).to include('late')
+    end
+
+    it { is_expected.to include('hot') }
+  end
+
+  describe [10, 20, 30] do
+    it 'checks for inclusion in the array, regardless of order' do
+      expect(subject).to include(10)
+      expect(subject).to include(30, 20)
+    end
+
+    it { is_expected.to include(20, 30, 10) }
+  end
+
+  describe ({ a: 2, b: 4 }) do
+    it 'can check for key existence' do
+      expect(subject).to include(:a)
+      expect(subject).to include(:a, :b)
+      expect(subject).to include(a: 2)
+    end
+
+    it { is_expected.to include(:b) }
+    it { is_expected.to include(b: 4) }
+  end
+end
